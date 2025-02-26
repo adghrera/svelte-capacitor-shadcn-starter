614e8e0f4dc74e31b343216cd377e56f09bd9583
Aman <adghrera@gmail.com>
2025-02-25 21:06:11 -0800

    capacitor live server on port 3000

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.aghrera.helloapp',
   appName: 'Hello App',
-  webDir: 'build'
+  webDir: 'build',
+  server: {
+    url: 'http://192.168.1.242:3000',
+    cleartext: true
+  }
 };
 
 export default config;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,19 @@
     import { Button } from "$lib/components/ui/button";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit 12345</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+    This is just so good!! &copy;
+    Just testing
+    Welcome to Sveltekit!!
+</div>
+
 <Button>Click me</Button>
+<Button>Click me 2</Button>
+<Button>Click me 2</Button>
+<Button>Click me 2</Button>
+<Button>Click me 2</Button>
+<Button>Click me 2</Button>
+<Button>Click me 2</Button>
+

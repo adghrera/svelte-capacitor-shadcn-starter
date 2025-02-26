d787a23f44c079e99195adaffc5b4be201c5e967
Aman <adghrera@gmail.com>
2025-02-25 19:26:45 -0800

    add shadcn

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	alias: {
-		'@/*': './src/lib/*'
+		'@/*': './src/*'
 	},
 	preprocess: vitePreprocess(),
 
